f9891b180720bd8fca5366f74d1f712f2e8b4b89
Erik Stevenson <eriknstevenson@gmail.com>
2016-05-19 06:01:44 -0400

    works on arbitrary csv files

--- site.hs
+++ site.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Monad
+import           Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import           Data.Char
 import           Data.Csv
 import           Data.Monoid
 import qualified Data.Text.Lazy as T
+import           Data.Vector (Vector)
 import qualified Data.Vector as V
 import           Lucid
 import           Hakyll
@@ -20,44 +23,33 @@ main = hakyll $ do
   match "csv/*.csv" $ do
     route $ setExtension "html" `composeRoutes` gsubRoute "csv/" (const "")
     compile $
-      ((fmap . fmap) (T.unpack . renderText) $ csvCompiler)
-      >>= (\table -> loadAndApplyTemplate "templates/layout.html" (csvContext <> defaultContext) table)
+      fmap (T.unpack . renderText) <$> csvCompiler
+      >>= loadAndApplyTemplate "templates/layout.html" (csvContext <> defaultContext)
       >>= relativizeUrls
 
   match "templates/*" $ compile templateCompiler
 
-data CSVFile = CSVFile
-  { courseID :: !Int
-  , title    :: !String
-  } deriving (Show)
-
-instance FromNamedRecord CSVFile where
-    parseNamedRecord r = CSVFile <$> r .: "id" <*> r .: "title"
 
 csvCompiler :: Compiler (Item (Html ()))
 csvCompiler = do
-  identifier <- filterIdentifier <$> getUnderlying
+  identifier <- makeTableID <$> getUnderlying
   getResourceLBS >>= withItemBody (csvToHtml identifier)
 
 csvToHtml :: String -> LBS.ByteString -> Compiler (Html ())
 csvToHtml identifier bs =
-  case decodeByName bs of
+  case decode NoHeader bs :: Either String Csv of
     Left err -> return mempty
-    Right (_, rows) -> return $
-    --cellspacing_ "0"
+    Right rows -> return $
       table_ [id_ (T.toStrict . T.pack $ identifier), class_ "display", width_ "100%"] $ do
-        thead_ $ tr_ $ do
-          th_ "id"
-          th_ "title"
+        thead_ $
+          makeRow (V.head rows)
         tbody_ $
-          V.forM_ rows makeRow
+          V.forM_ (V.tail rows) makeRow
 
-makeRow :: CSVFile -> Html ()
-makeRow row = tr_ $ do
-  td_ $ toHtml . show . courseID $ row
-  td_ $ toHtml . title $ row
+makeRow :: Vector ByteString -> Html ()
+makeRow row = tr_ $ V.forM_ row $ \column -> td_ $ toHtml . BS.unpack $ column
 
-csvContext = field "table-id" $ return . filterIdentifier . itemIdentifier
+csvContext = field "table-id" $ return . makeTableID . itemIdentifier
 
-filterIdentifier = filter isLetter . show
+makeTableID = filter isLetter . show
 
